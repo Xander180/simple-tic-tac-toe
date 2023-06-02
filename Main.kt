7dd90faeae74dd14d73f839a69b8d1588e54a54f
Xander180 <wilson.ramirez98@gmail.com>
2023-06-02 10:29:00 -0400

    --Create win conditions for "O"     -"OOO" is true (including diagonals)     -winO is true and winX is false --Add win condition for "X"     -winX is true and winO is false

--- Main.kt
+++ Main.kt
@@ -35,6 +35,18 @@ fun main() {
     if (input[0].toString() + input[4] + input[8] == "XXX") winX = true
     if (input[2].toString() + input[4] + input[6] == "XXX") winX = true
 
+    if (input.subList(0, 3).joinToString("") == "OOO") winO = true
+    if (input.subList(3, 6).joinToString("") == "OOO") winO = true
+    if (input.subList(6, 9).joinToString("") == "OOO") winO = true
+    if (input[0].toString() + input[3] + input[6] == "OOO") winO = true
+    if (input[1].toString() + input[4] + input[7] == "OOO") winO = true
+    if (input[2].toString() + input[5] + input[8] == "OOO") winO = true
+    if (input[0].toString() + input[4] + input[8] == "OOO") winO = true
+    if (input[2].toString() + input[4] + input[6] == "OOO") winO = true
+
+    if (winX && !winO) println("X wins")
+    if (!winX && winO) println("O wins")
+
 
 
 
@@ -44,20 +56,19 @@ fun main() {
 
 /*
 1. Create "Game not finished" condition
-    a. Neither X nor O == 3
-    b. There are "_" on board
+    a. Not impossible
+    b. When neither winX nor winO == true AND board NOT filled
 2. Create Draw condition
-    a. When neither X nor O == 3
-    b. Board is filled
+    a. When neither winX nor winO == true AND board is filled
 3. Create X Wins Condition
-    a. X count == 3
-    b. O count != 3
-    c. "XXX" is true (including diagonals) -- DONE
+    a. X count >= 3
+    b. winX == true && winO == false -- DONE (2)
+    c. "XXX" is true (including diagonals) -- DONE(1)
 4. Create O wins condition
-    a. O count == 3
-    b. X count != 3
-    c. "OOO" is true (including diagonals)
+    a. O count >= 3
+    b. winO == true && winX == false -- DONE (2)
+    c. "OOO" is true (including diagonals) -- DONE(2)
 5. Create impossible condition
-    a. countX - countO is not between -1 and 1
-    b. countX and countO == 3
+    a. countX - countO is not between -1 and 1 -- DONE (2)
+    b. winX and winO == true
  */
